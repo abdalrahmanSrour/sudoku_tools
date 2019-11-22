55f9ee37eb30fa2236b49f186da3620ba2e3bea6
abdalrahmanSrour <eng.abdsrour@gmail.com>
2019-11-22 21:35:24 +0200

    Add SudokuValidator implementation

--- src/SudokuValidator.c
+++ src/SudokuValidator.c
@@ -1,21 +1,95 @@
+#include "../inc/Common.h"
 #include "../inc/SudokuGrid.h"
 
 unsigned char verboseMode = 0;
 const char *filename = NULL;
 const char *appname = NULL;
 
-int validateSudokuGrid(int isFile, char *filename)
+#define MAX_BUF_SIZE (128 * 64) 
+
+void readGridFrom(FILE *file, char *buf, size_t maxSize)
 {
-    // TODO: read file content, or read from stdin
+    size_t line_size, totalR = 0, chars;
 
-    // TODO: parse grid, and create SudokuGrid object
+    int ii = 0;
+    while (true)
+    {
+        char line[128];
+        if (NULL == fgets(line, 128, file))
+            break;
 
-    // TODO: Validate grid
+        chars = strlen(line);
 
-    // TODO: Show result
+        // if (chars == 0) break;
+        if (!filename)
+        {
+            if (chars == 1 && (line[0] == '\n' || line[0] == '\r')) break;
+            if (chars == 2 && !strcmp(line, "\r\n")) break;
+        }
+        
+        sprintf(buf + totalR, "%s\n", line);
+        totalR = strlen(buf);
+    }
 }
 
-void showArgsMessage()
+int validateSudokuGrid(void)
+{
+    char buf[MAX_BUF_SIZE];
+    DBG("Reading data");
+
+    if (filename)
+    {
+        FILE *f = fopen(filename, "r");
+        if (!f)
+        {
+            printf("Unable to open file %s\n", filename);
+        }
+        readGridFrom(f, buf, MAX_BUF_SIZE);
+    }
+    else
+    {
+        printf("No file provided, using stdin...\n");
+        printf("Enter each row as one line\nempty line will terminate input\n");
+        readGridFrom(stdin, buf, MAX_BUF_SIZE);
+    }
+
+    DBGF("Read buf:\n%s\n", buf);
+
+    // TODO: parse grid, and create SudokuGrid object
+    Sudoku_Grid *grid = sudoku_grid_create_from_str(buf);
+
+    if (!grid)
+    {
+        printf("Invalid input :(\ncouldn't parse input\nfor more info use (-v) flag\n");
+        exit(1);
+    }
+
+    // TODO: Validate grid
+    if (!sudoku_grid_is_valid(grid))
+    {
+        printf("Invalid input :(\nProvided Sudoku Grid is not valid\nNot matching game standards\nfor more info use (-v) flag\n");
+        exit(1);
+    }
+
+    // TODO: Show result
+    char *buff = sudoku_grid_print(grid);
+    
+    if (buff)
+    {
+        printf("Input Sudoku grid is valid!! horaay :)\n");
+        printf("Here is parsed board\n%s\n", buff);
+
+        free(buff);
+    } else
+    {
+        printf("Invalid input :(\nProvided Sudoku Grid is not valid\nNot matching game standards\nfor more info use (-v) flag\n");
+        exit(1);
+    }
+
+    DBG("Thank you!");
+}
+
+void showArgsMessage(void)
 {
     puts("Sudoku grid validator, made by Abed Srour <eng.abdsrour@gmail.com>");
     puts("---------------------------------------------------------------");
@@ -57,8 +131,8 @@ int main(int argc, char const *argv[])
 {
     parseArgs(argc, argv);
 
-    DBGF("Args result: appname=%s filename=%s v=%d", appname, filename, verboseMode);
+    DBGF("Args result: appname=%s filename=%s v=%d\n", appname, filename, verboseMode);
 
-
+    validateSudokuGrid();
     return 0;
 }
